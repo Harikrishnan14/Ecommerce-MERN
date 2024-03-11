e9679f54e488fb1d6c3eb2b2bb43c5307e13c8c0
Harikrishnan14 <harikrishnankhd09@gmail.com>
2024-03-11 21:59:55 +0530

    Filtering the products

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -34,7 +34,15 @@ const getAllProduct = asyncHandler(async (req, res) => {
         let queryStr = JSON.stringify(queryObj)
         queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
 
-        const query = Product.find(JSON.parse(queryStr))
+        let query = Product.find(JSON.parse(queryStr))
+
+        if (req.query.sort) {
+            const sortBy = req.query.sort.split(',').join(' ')
+            query = query.sort(sortBy)
+        } else {
+            query = query.sort('-createdAt')
+        }
+
         const products = await query
         res.json(products)
     } catch (error) {
