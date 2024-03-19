de69e4897cf7d91ec051c50b8086d1454003bec2
Harikrishnan14 <harikrishnankhd09@gmail.com>
2024-03-19 21:44:11 +0530

    Endpoint to like a blog

--- Backend/controllers/blogController.js
+++ Backend/controllers/blogController.js
@@ -15,6 +15,7 @@ const createBlog = asyncHandler(async (req, res) => {
 
 const updateBlog = asyncHandler(async (req, res) => {
     const { id } = req.params
+    validateMongoID(id)
     try {
         const updatedBlog = await Blog.findByIdAndUpdate(id, req.body, { new: true })
         res.json(updatedBlog)
@@ -25,6 +26,7 @@ const updateBlog = asyncHandler(async (req, res) => {
 
 const getBlog = asyncHandler(async (req, res) => {
     const { id } = req.params
+    validateMongoID(id)
     try {
         const fetchedBlog = await Blog.findById(id)
         await Blog.findByIdAndUpdate(id, { $inc: { numViews: 1 } }, { new: true })
@@ -45,6 +47,7 @@ const getAllBlogs = asyncHandler(async (req, res) => {
 
 const deleteBlog = asyncHandler(async (req, res) => {
     const { id } = req.params
+    validateMongoID(id)
     try {
         const deletedBlog = await Blog.findByIdAndDelete(id)
         res.json(deletedBlog)
@@ -53,5 +56,37 @@ const deleteBlog = asyncHandler(async (req, res) => {
     }
 })
 
+const likeBlog = asyncHandler(async (req, res) => {
+    const { blogId } = req.body
+    console.log(blogId);
+    // validateMongoID(blogId)
+    const blog = await Blog.findById(blogId)
+    const loginUserId = req?.user?._id
+    const isLiked = blog?.isLiked
+    const alreadyDisliked = blog?.dislikes?.find((userId) => userId?.toString() === loginUserId.toString())
 
-module.exports = { createBlog, updateBlog, getBlog, getAllBlogs, deleteBlog }
+    if (alreadyDisliked) {
+        const blog = await Blog.findByIdAndUpdate(blogId, {
+            $pull: { dislikes: loginUserId },
+            isDisliked: false
+        }, { new: true })
+        res.json(blog)
+    }
+
+    if (isLiked) {
+        const blog = await Blog.findByIdAndUpdate(blogId, {
+            $pull: { likes: loginUserId },
+            isLiked: false
+        }, { new: true })
+        res.json(blog)
+    } else {
+        const blog = await Blog.findByIdAndUpdate(blogId, {
+            $push: { likes: loginUserId },
+            isLiked: true
+        }, { new: true })
+        res.json(blog)
+    }
+})
+
+
+module.exports = { createBlog, updateBlog, getBlog, getAllBlogs, deleteBlog, likeBlog }
--- Backend/routes/blogRoute.js
+++ Backend/routes/blogRoute.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const { authMiddleware, isAdmin } = require('../middlewares/authMiddleware')
-const { createBlog, updateBlog, getBlog, getAllBlogs, deleteBlog } = require('../controllers/blogController')
+const { createBlog, updateBlog, getBlog, getAllBlogs, deleteBlog, likeBlog } = require('../controllers/blogController')
 
 
 router.post('/', authMiddleware, isAdmin, createBlog)
+router.put('/like', authMiddleware, likeBlog)
 router.put('/:id', authMiddleware, isAdmin, updateBlog)
 router.get('/:id', getBlog)
 router.get('/', getAllBlogs)
