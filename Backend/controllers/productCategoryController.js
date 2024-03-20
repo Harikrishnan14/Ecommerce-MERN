150c48b2a701bec51c9dac437bb7c6f5a47533e8
Harikrishnan14 <harikrishnankhd09@gmail.com>
2024-03-20 22:49:59 +0530

    Endpoints to get a single category and all categories

--- Backend/controllers/productCategoryController.js
+++ Backend/controllers/productCategoryController.js
@@ -34,5 +34,25 @@ const deleteCategory = asyncHandler(async (req, res) => {
     }
 })
 
+const getACategory = asyncHandler(async (req, res) => {
+    const { id } = req.params
+    validateMongoID(id)
+    try {
+        const category = await Category.findById(id)
+        res.json(category)
+    } catch (error) {
+        throw new Error(error)
+    }
+})
 
-module.exports = { createCategory, updateCategory, deleteCategory }
+const getAllCategory = asyncHandler(async (req, res) => {
+    try {
+        const categories = await Category.find()
+        res.json(categories)
+    } catch (error) {
+        throw new Error(error)
+    }
+})
+
+
+module.exports = { createCategory, updateCategory, deleteCategory, getACategory, getAllCategory }
--- Backend/routes/productCategoryRoute.js
+++ Backend/routes/productCategoryRoute.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const router = express.Router()
 const { authMiddleware, isAdmin } = require('../middlewares/authMiddleware')
-const { createCategory, updateCategory, deleteCategory } = require('../controllers/productCategoryController')
+const { createCategory,
+    updateCategory,
+    deleteCategory,
+    getACategory,
+    getAllCategory
+} = require('../controllers/productCategoryController')
 
 
 router.post('/', authMiddleware, isAdmin, createCategory)
 router.put('/:id', authMiddleware, isAdmin, updateCategory)
 router.delete('/:id', authMiddleware, isAdmin, deleteCategory)
+router.get('/:id', getACategory)
+router.get('/', getAllCategory)
 
 
 module.exports = router
