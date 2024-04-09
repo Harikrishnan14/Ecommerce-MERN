8e54640fc6766da3fce51f287d3088c2dc70a567
Harikrishnan14 <harikrishnankhd09@gmail.com>
2024-04-09 22:04:01 +0530

    Homepage third part

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -102,6 +102,72 @@ const Home = () => {
                     </div>
                 </div>
             </section>
+            <section className="home-wrapper-2 py-5">
+                <div className="container-xxl">
+                    <div className="row">
+                        <div className="col-12">
+                            <div className="categories d-flex flex-wrap justify-content-between align-items-center">
+                                <div className='d-flex gap align-items-center'>
+                                    <div>
+                                        <h6>Cameras</h6>
+                                        <p>10 items</p>
+                                    </div>
+                                    <img src="images/camera.jpg" alt="camera" />
+                                </div>
+                                <div className='d-flex gap align-items-center'>
+                                    <div>
+                                        <h6>Smart TV</h6>
+                                        <p>10 items</p>
+                                    </div>
+                                    <img src="images/tv.jpg" alt="camera" />
+                                </div>
+                                <div className='d-flex gap align-items-center'>
+                                    <div>
+                                        <h6>Smart Watches</h6>
+                                        <p>10 items</p>
+                                    </div>
+                                    <img src="images/headphone.jpg" alt="camera" />
+                                </div>
+                                <div className='d-flex gap align-items-center'>
+                                    <div>
+                                        <h6>Music & Gaming</h6>
+                                        <p>10 items</p>
+                                    </div>
+                                    <img src="images/camera.jpg" alt="camera" />
+                                </div>
+                                <div className='d-flex gap align-items-center'>
+                                    <div>
+                                        <h6>Cameras</h6>
+                                        <p>10 items</p>
+                                    </div>
+                                    <img src="images/camera.jpg" alt="camera" />
+                                </div>
+                                <div className='d-flex gap align-items-center'>
+                                    <div>
+                                        <h6>Smart TV</h6>
+                                        <p>10 items</p>
+                                    </div>
+                                    <img src="images/tv.jpg" alt="camera" />
+                                </div>
+                                <div className='d-flex gap align-items-center'>
+                                    <div>
+                                        <h6>Smart Watches</h6>
+                                        <p>10 items</p>
+                                    </div>
+                                    <img src="images/headphone.jpg" alt="camera" />
+                                </div>
+                                <div className='d-flex gap align-items-center'>
+                                    <div>
+                                        <h6>Music & Gaming</h6>
+                                        <p>10 items</p>
+                                    </div>
+                                    <img src="images/camera.jpg" alt="camera" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
     )
 }
