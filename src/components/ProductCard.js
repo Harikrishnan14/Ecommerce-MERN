773783ad2d424a3cd5b277e5375de039426e1dd5
Harikrishnan14 <harikrishnankhd09@gmail.com>
2024-04-12 21:55:40 +0530

    Special products

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -22,7 +22,7 @@ const ProductCard = () => {
                     <h5 className="product-title">
                         Kids headphone bulk 10 pack mutli colored for students
                     </h5>
-                    <ReactStars count={5} size={24} value="3" edit={false} activeColor="#ffd700" />
+                    <ReactStars count={5} size={24} value={3} edit={false} activeColor="#ffd700" />
                     <p className="price">
                         $100.00
                     </p>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom'
 import Marquee from "react-fast-marquee";
 import BlogCard from '../components/BlogCard';
 import ProductCard from '../components/ProductCard';
+import SpecialProduct from '../components/SpecialProduct';
 
 const Home = () => {
     return (
@@ -183,6 +184,23 @@ const Home = () => {
                         <ProductCard />
                         <ProductCard />
                         <ProductCard />
+                    </div>
+                </div>
+            </section>
+            <section className="special-wrapper home-wrapper-2 py-5">
+                <div className="container-xxl">
+                    <div className="row">
+                        <div className="col-12">
+                            <h3 className="section-heading">
+                                Special Products
+                            </h3>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <SpecialProduct />
+                        <SpecialProduct />
+                        <SpecialProduct />
+                        <SpecialProduct />
                     </div>
                 </div>
             </section>
