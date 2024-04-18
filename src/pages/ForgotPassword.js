3ff88620474a39f060bd9cc2ee3197b245b2f6d3
Harikrishnan14 <harikrishnankhd09@gmail.com>
2024-04-18 22:07:52 +0530

    Forgot password page

--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -3,30 +3,25 @@ import MetaTags from '../components/MetaTags'
 import BreadCrumb from '../components/BreadCrumb'
 import { Link } from 'react-router-dom'
 
-const Login = () => {
+const ForgotPassword = () => {
     return (
         <div>
-            <MetaTags title="Login" />
-            <BreadCrumb title="Login" />
+            <MetaTags title="Forgot Password" />
+            <BreadCrumb title="Forgot Password" />
             <div className="login-wrapper home-wrapper-2 py-5">
                 <div className="container-xxl">
                     <div className="row">
                         <div className="col-12">
                             <div className="auth-card">
-                                <h3 className='text-center mb-4'>Login</h3>
+                                <h3 className='text-center mb-3'>Reset Your Password</h3>
+                                <p className="text-center mt-2 mb-4">We will send you an email to reset your password</p>
                                 <form action="" className='d-flex flex-column gap-15'>
                                     <div>
                                         <input type="email" name="email" placeholder="Email" className="form-control" />
                                     </div>
-                                    <div className='mt-2'>
-                                        <input type="password" name="password" placeholder="Password" className="form-control" />
-                                    </div>
-                                    <div>
-                                        <Link to='/forgot-password'>Forgot Password?</Link>
-                                    </div>
-                                    <div className='d-flex justify-content-center align-items-center gap-15 mt-3'>
-                                        <button className='button border-0'>Login</button>
-                                        <Link to='/' className='button signup'>Sign Up</Link>
+                                    <div className='d-flex justify-content-center flex-column align-items-center gap-15 mt-3'>
+                                        <button className='button border-0' type='submit'>Submit</button>
+                                        <Link to='/login'>Cancel</Link>
                                     </div>
                                 </form>
                             </div>
@@ -38,4 +33,4 @@ const Login = () => {
     )
 }
 
-export default Login
+export default ForgotPassword
