5c02981c057c515aee831d9734bfbd7f63b266ff
Harikrishnan14 <harikrishnankhd09@gmail.com>
2024-04-25 22:37:10 +0530

    Single product page desc - 1

--- src/pages/SingleProduct.js
+++ src/pages/SingleProduct.js
@@ -4,6 +4,9 @@ import BreadCrumb from '../components/BreadCrumb'
 import ProductCard from '../components/ProductCard';
 import ReactStars from "react-rating-stars-component";
 import ReactImageZoom from 'react-image-zoom';
+import Color from '../components/Color'
+import { IoShuffle } from "react-icons/io5";
+import { AiOutlineHeart } from "react-icons/ai";
 
 const SingleProduct = () => {
 
@@ -11,8 +14,8 @@ const SingleProduct = () => {
 
     const props = {
         width: 400,
-        height: 500,
-        zoomWidth: 500,
+        height: 600,
+        zoomWidth: 600,
         img: "https://media.wired.com/photos/6511aab1189c419c40374c92/1:1/w_1358,h_1358,c_limit/Apple-Watch-Ultra-2-Alt-Gear.jpg"
     };
 
@@ -22,14 +25,14 @@ const SingleProduct = () => {
             <BreadCrumb title="Product Name" />
             <div className="main-product-wrapper home-wrapper-2 py-5">
                 <div className="container-xxl">
-                    <div className="row">
+                    <div className="row main-product-inner-wrapper">
                         <div className="col-6">
                             <div className="main-product-image">
                                 <div>
                                     <ReactImageZoom {...props} />
                                 </div>
                             </div>
-                            <div className="other-product-images d-flex flex-wrap gap-15">
+                            <div className="other-product-images d-flex justify-content-between flex-wrap gap-15 py-4">
                                 <div>
                                     <img src="https://media.wired.com/photos/6511aab1189c419c40374c92/1:1/w_1358,h_1358,c_limit/Apple-Watch-Ultra-2-Alt-Gear.jpg" alt="" className='img-fluid' />
                                 </div>
@@ -44,7 +47,75 @@ const SingleProduct = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className="col-6"></div>
+                        <div className="col-6">
+                            <div className="main-product-details">
+                                <div className='border-bottom'>
+                                    <h3 className='title'>APPLE Watch Series 2 – 42 Mm Stainless Steel Case</h3>
+                                </div>
+                                <div className="border-bottom py-3">
+                                    <p className="price">$100</p>
+                                    <div className='d-flex align-items-center gap-10'>
+                                        <ReactStars count={5} size={24} value={3} edit={false} activeColor="#ffd700" />
+                                        <p className='mb-0 t-review'>(2 reviews)</p>
+                                    </div>
+                                    <a className='review-btn' href="#review">Write a Review</a>
+                                </div>
+                                <div className="border-bottom py-3">
+                                    <div className="d-flex align-items-center gap-10 my-2">
+                                        <h3 className='product-heading'>Type :</h3>
+                                        <p className='product-data'>Watch</p>
+                                    </div>
+                                    <div className="d-flex align-items-center gap-10 my-2">
+                                        <h3 className='product-heading'>Brand :</h3>
+                                        <p className='product-data'>Apple</p>
+                                    </div>
+                                    <div className="d-flex align-items-center gap-10 my-2">
+                                        <h3 className='product-heading'>Categories :</h3>
+                                        <p className='product-data'>SmartWatch, Watch</p>
+                                    </div>
+                                    <div className="d-flex align-items-center gap-10 my-2">
+                                        <h3 className='product-heading'>Tags :</h3>
+                                        <p className='product-data'>Apple, Watch Series 7</p>
+                                    </div>
+                                    <div className="d-flex align-items-center gap-10 my-2">
+                                        <h3 className='product-heading'>Availability :</h3>
+                                        <p className='product-data'>In Stock</p>
+                                    </div>
+                                    <div className="d-flex flex-column gap-10 mt-2 mb-3">
+                                        <h3 className='product-heading'>Size :</h3>
+                                        <div className="d-flex flex-wrap gap-10">
+                                            <span className="badge border border-1 bg-white text-dark border-secondary">S</span>
+                                            <span className="badge border border-1 bg-white text-dark border-secondary">M</span>
+                                            <span className="badge border border-1 bg-white text-dark border-secondary">L</span>
+                                            <span className="badge border border-1 bg-white text-dark border-secondary">XL</span>
+                                            <span className="badge border border-1 bg-white text-dark border-secondary">XXL</span>
+                                        </div>
+                                    </div>
+                                    <div className="d-flex flex-column gap-10 mt-2 mb-3">
+                                        <h3 className='product-heading'>Color :</h3>
+                                        <Color />
+                                    </div>
+                                    <div className="d-flex align-items-center flex-row gap-15 mt-2 mb-3">
+                                        <h3 className='product-heading'>Quantity :</h3>
+                                        <div className=''>
+                                            <input type="number" defaultValue={1} min={1} max={10} style={{ width: "70px" }} className='form-control' />
+                                        </div>
+                                        <div className='d-flex align-items-center gap-30 ms-5'>
+                                            <button className='button border-0'>Add to Cart</button>
+                                            <button className='button signup border-0'>Buy Now</button>
+                                        </div>
+                                    </div>
+                                    <div className="d-flex align-items-center gap-30">
+                                        <div>
+                                            <a href=""><IoShuffle className='fs-5 me-2' /> Add to Compare</a>
+                                        </div>
+                                        <div>
+                                            <a href=""><AiOutlineHeart className='fs-5 me-2' /> Add to Wishlist</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -60,7 +131,7 @@ const SingleProduct = () => {
                     </div>
                 </div>
             </section>
-            <section className="reviews-wrapper home-wrapper-2">
+            <section id='review' className="reviews-wrapper home-wrapper-2">
                 <div className="container-xxl">
                     <div className="row">
                         <div className="col-12">
