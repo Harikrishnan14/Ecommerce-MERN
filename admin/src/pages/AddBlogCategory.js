b74fe9e14f5378988f25b6c3121f959009bdc783
Harikrishnan14 <harikrishnankhd09@gmail.com>
2024-05-16 20:57:31 +0530

    AdminDashboard - AddBlogCategory page

--- admin/src/pages/AddBlogCategory.js
+++ admin/src/pages/AddBlogCategory.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import CustomInput from '../components/CustomInput'
 
 const AddBlogCategory = () => {
     return (
         <div>
-            AddBlogCategory
+            <h3 className="mb-4">Add Blog Category</h3>
+            <div>
+                <form action="">
+                    <CustomInput type="text" label="Enter Blog Category" />
+                    <button type='submit' className='btn btn-success border-0 rounded-3 my-4'>Add Blog Category</button>
+                </form>
+            </div>
         </div>
     )
 }
