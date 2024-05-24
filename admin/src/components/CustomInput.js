0a81ebebd271a0af073873f0723bbc4cb1b3f055
Harikrishnan14 <harikrishnankhd09@gmail.com>
2024-05-24 22:07:45 +0530

    Login - API integration setup 2

--- admin/src/components/CustomInput.js
+++ admin/src/components/CustomInput.js
@@ -2,11 +2,20 @@ import React from 'react'
 
 const CustomInput = (props) => {
 
-    const { type, label, i_id, i_class } = props
+    const { type, label, i_id, i_class, name, val, onChange, onBlur } = props
 
     return (
         <div className="form-floating mb-3">
-            <input type={type} className={`form-control ${i_class}`} id={i_id} placeholder={label} />
+            <input
+                type={type}
+                className={`form-control ${i_class}`}
+                id={i_id}
+                placeholder={label}
+                name={name}
+                value={val}
+                onChange={onChange}
+                onBlur={onChange}
+            />
             <label htmlFor={label}>{label}</label>
         </div>
     )
--- admin/src/pages/Login.js
+++ admin/src/pages/Login.js
@@ -1,20 +1,32 @@
 import React from 'react'
 import CustomInput from '../components/CustomInput'
 import { Link } from 'react-router-dom'
+import { useFormik } from 'formik'
 
 const Login = () => {
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: ''
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
+
     return (
         <div className='py-5' style={{ background: "#ffd333", minHeight: "100vh", display: "flex", justifyContent: "center", alignItems: "center" }}>
             <div className='my-5 w-25 bg-white rounded-3 mx-auto p-4'>
                 <h3 className='text-center title'>Login</h3>
                 <p className='text-center'>Login to you account to continue</p>
-                <form action="">
-                    <CustomInput type='text' label='Email Address' i_id='email' />
-                    <CustomInput type='password' label='Password' i_id='password' />
+                <form action="" onSubmit={formik.handleSubmit}>
+                    <CustomInput type='text' name="email" val={formik.values.email} label='Email Address' i_id='email' onChange={formik.handleChange('email')} />
+                    <CustomInput type='password' name="password" val={formik.values.password} label='Password' i_id='password' onChange={formik.handleChange('password')} />
                     <div className='mb-3 text-end'>
                         <Link to='/forgot-password'>Forgot Password?</Link>
                     </div>
-                    <Link to='/admin' className='border-0 px-3 py-2 text-white fw-bold w-100 text-center text-decoration-none fs-5' type='submit' style={{ background: "#ffd333" }}>Login</Link>
+                    <button className='border-0 px-3 py-2 text-white fw-bold w-100 text-center text-decoration-none fs-5' type='submit' style={{ background: "#ffd333" }}>Login</button>
                 </form>
             </div>
         </div>
