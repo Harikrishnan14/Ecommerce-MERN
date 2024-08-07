894d4e9d97bae2948c3e28a718aa7e66c8a53274
Harikrishnan14 <harikrishnankhd09@gmail.com>
2024-08-07 22:42:53 +0530

    Fetching and displaying featured products

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -197,10 +197,48 @@ const Home = () => {
                             Featured Collection
                         </h3>
                     </div>
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
-                    <ProductCard />
+                    {productState?.filter((item) => item.tags === 'featured')?.map((item, index) => {
+                        return (
+                            <div className="col-3" key={index}>
+                                <Link to='' className="product-card position-relative">
+                                    <div className="wishlist-icon position-absolute">
+                                        <button className='border-0 bg-transparent' onClick={() => addToWish(item?._id)}>
+                                            <img src={Wish} alt="wishlist" />
+                                        </button>
+                                    </div>
+                                    <div className="product-image">
+                                        <img src={item?.images[0]?.url} className='img-fluid' alt="product" />
+                                        <img src={Watch2} className='img-fluid' alt="product" />
+                                    </div>
+                                    <div className="product-details">
+                                        <h6 className="brand">
+                                            {item?.brand}
+                                        </h6>
+                                        <h5 className="product-title">
+                                            {item?.title}
+                                        </h5>
+                                        <ReactStars count={5} size={24} value={Number(item?.totalrating)} edit={false} activeColor="#ffd700" />
+                                        <p className="price">
+                                            ${item?.price}
+                                        </p>
+                                    </div>
+                                    <div className="action-bar position-absolute">
+                                        <div className="d-flex flex-column gap-15">
+                                            <button className='border-0 bg-transparent'>
+                                                <img src={ProdCompare} alt="compare" />
+                                            </button>
+                                            <button className='border-0 bg-transparent'>
+                                                <img src={View} alt="view" />
+                                            </button>
+                                            <button className='border-0 bg-transparent'>
+                                                <img src={AddToCart} alt="add-to-cart" />
+                                            </button>
+                                        </div>
+                                    </div>
+                                </Link>
+                            </div>
+                        )
+                    })}
                 </div>
             </Container>
             <Container class1='famous-wrapper home-wrapper-2 py-5'>
@@ -280,7 +318,7 @@ const Home = () => {
                     </div>
                 </div>
                 <div className="row">
-                    {productState?.map((item, index) => {
+                    {productState?.filter((item) => item.tags === 'popular')?.map((item, index) => {
                         return (
                             <div className="col-3" key={index}>
                                 <Link to='' className="product-card position-relative">
