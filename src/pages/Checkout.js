00c93ef3173b9d1f3c62570669d1018dade1d4c0
Harikrishnan14 <harikrishnankhd09@gmail.com>
2024-08-14 23:54:28 +0530

    Displaying cart items on Checkout page

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { IoIosArrowBack } from "react-icons/io";
-import Watch from '../images/watch.jpg'
 import Container from '../components/Container';
+import { useSelector } from 'react-redux';
 
 const Checkout = () => {
+
+    const [totalAmount, setTotalAmount] = useState(0)
+
+    const cartState = useSelector((state) => state.auth.userCart)
+
+    useEffect(() => {
+        let sum = 0
+        for (let index = 0; index < cartState?.length; index++) {
+            sum = sum + (Number(cartState[index]?.quantity) * cartState[index]?.price)
+            setTotalAmount(sum)
+        }
+    }, [cartState])
+
     return (
         <div>
             <Container class1='checkout-wrapper home-wrapper-2 py-5'>
@@ -77,35 +90,37 @@ const Checkout = () => {
                     </div>
                     <div className="col-5">
                         <div className='border-bottom py-4'>
-                            <div className="d-flex gap-10 align-items-center mb-2">
-                                <div className='w-75 d-flex gap-10'>
-                                    <div className='w-25 position-relative'>
-                                        <span className=' badge bg-secondary text-white rounded-circle p-2 position-absolute' style={{ top: "-10px", right: "-5px" }}>1</span>
-                                        <img src={Watch} alt="" className='img-fluid' />
+                            {cartState?.map((item, index) => (
+                                <div className="d-flex gap-10 align-items-center mb-2" key={index}>
+                                    <div className='w-75 d-flex gap-10'>
+                                        <div className='w-25 position-relative'>
+                                            <span className=' badge bg-secondary text-white rounded-circle p-2 position-absolute' style={{ top: "-10px", right: "-5px" }}>{item?.quantity}</span>
+                                            <img src={item?.productID?.images[0]?.url} width={100} height={100} alt="" />
+                                        </div>
+                                        <div>
+                                            <h5 className="total-price">{item?.productID?.title}</h5>
+                                            <p className='total-price'>{item?.color?.title}</p>
+                                        </div>
                                     </div>
-                                    <div>
-                                        <h5 className="total-price">Watch series 7</h5>
-                                        <p className='total-price'>Sample desc</p>
+                                    <div className='flex-grow-1'>
+                                        <h5 className='total'>${item?.price * item?.quantity}</h5>
                                     </div>
                                 </div>
-                                <div className='flex-grow-1'>
-                                    <h5 className='total'>$100</h5>
-                                </div>
-                            </div>
+                            ))}
                         </div>
                         <div className='border-bottom py-4'>
                             <div className='d-flex align-items-center justify-content-between'>
                                 <p className='total'>Subtotal</p>
-                                <p className='total-price'>$399</p>
+                                <p className='total-price'>${totalAmount ? totalAmount : 0}</p>
                             </div>
                             <div className='d-flex align-items-center justify-content-between'>
                                 <p className='mb-0 total'>Shipping</p>
-                                <p className='mb-0 total-price'>$19</p>
+                                <p className='mb-0 total-price'>$5</p>
                             </div>
                         </div>
                         <div className='d-flex align-items-center justify-content-between py-4'>
                             <h4 className='total'>Total</h4>
-                            <h5 className='total-price'>$418</h5>
+                            <h5 className='total-price'>${totalAmount ? totalAmount + 5 : 0}</h5>
                         </div>
                     </div>
                 </div>
